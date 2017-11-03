bc9e173379ca1276589d4924b965bf5b7e10cead
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-03 20:32:59 +0700

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onSignIn(x) {
-    console.log(x);
+  onSignIn(formSignIn) {
+    console.log(formSignIn);
   }
 }
